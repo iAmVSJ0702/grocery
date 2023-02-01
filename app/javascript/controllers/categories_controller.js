52d3326bc011ce9a0f57c729c92ef794012c5ee7
iAmVSJ0702 <vsj0702@gmail.com>
2023-02-01 15:35:44 +0530

    Turbo Stream Working

--- app/javascript/controllers/categories_controller.js
+++ app/javascript/controllers/categories_controller.js
@@ -12,5 +12,14 @@ export default class extends Controller {
 
   loadCategorySubcategories(){
     const selectedCategory = this.element.options[this.element.selectedIndex].value
+    this.url = `/admin/fetch_category_subcategories?category_code=${selectedCategory}`
+    fetch(this.url,{
+      headers: {
+        Accept: "text/vnd.turbo-stream.html"
+      }
+    })
+    .then(response => response.text())
+    .then(html=> Turbo.renderStreamMessage(html))
+    
   }
 }
