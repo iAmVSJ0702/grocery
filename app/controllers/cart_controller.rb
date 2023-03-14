ef7f77ae7fc7efaae0d9b929443125a6e813a5c3
iAmVSJ0702 <vsj0702@gmail.com>
2023-03-14 16:43:30 +0530

    Bug Fix in cart

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -7,17 +7,20 @@ class CartController < ApplicationController
   end
 
   def add
+    @message = "successfully added to cart"
+    @url = cart_url
     @item = Item.find_by(id: params[:id])
     quantity = params[:quantity].to_i
     current_orderable = @cart.orderables.find_by(item_id: @item.id)
     if current_orderable && quantity > 0
       current_orderable.update(quantity:)
-    elsif quantity <= 0
-      current_orderable.destroy
-    else
+    elsif quantity > 0
       @cart.orderables.create(item: @item, quantity:)
+    else
+      @message = "Quantity of item was left blank, please enter a valid number"
+      @url = item_path(@item)
     end
-      redirect_to cart_url, notice: "successfully added to cart"
+      redirect_to @url, notice: @message
   end
 
   def remove
